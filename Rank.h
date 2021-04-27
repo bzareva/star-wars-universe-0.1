2d90a9553df660a37a1a887b11833ec73eee409c
Blagovesta Zareva <blagovesta_zareva99@abv.bg>
2021-04-27 14:33:32 +0300

    separating enum class Rank from Jedi because i use it in String class

---
+++ Rank.h
@@ -0,0 +1,19 @@
+#ifndef __RANK_H__
+#define __RANK_H__
+/*!
+ * Rank of jedi is enum class.
+ * Contains the possible rank, that a jedi can have.
+ * Ranks are arranged in ascending order- 1, 2,.., 8.
+ */
+enum class Rank {
+	YOUNGLING = 1,
+	INITIATE  = 2,
+	PADAWAN   = 3,
+	KNIGHT_ASPIRANT = 4,
+	KNIGHT = 5,
+	MASTER = 6,
+	BATTLE_MASTER = 7,
+	GRAND_MASTER  = 8
+};
+
+#endif //__RANK_H__
